c145f15c0e1f2cf9de2bcdfa333a44b64a8f0ed9
Adrian Sharpe <adriansharpe148@gmail.com>
2023-01-25 17:42:51 -0500

    gitignore updates + use local settings env

--- fantasybball/fantasybball/asgi.py
+++ fantasybball/fantasybball/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasybball.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantasybball.local")
 
 application = get_asgi_application()
